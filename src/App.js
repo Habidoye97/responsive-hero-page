22ff0b5a57e47494ac17bf033438ed03d8160351
Habidoye97 <abidoyeabiodun97@gmail.com>
2022-11-05 18:31:17 +0100

    Update app component, app.css and create navbar.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
 import './App.css';
+import Elipse1 from './images/ellipse2.png'
 
 function App() {
   return (
     <div className="header">
       <div className='header-left'>
-        me
+        <div className="logo_container">
+          <div className="logo"></div>
+          <h1 className="logo_description">Interior Thinkers</h1>
+        </div>
+
+        <div className='interior-page-head'>
+          <h3>Make Your Interior more minimalistic</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec purus sodales vel facilisis amet. Fringilla viverra euismod risus vel nisl, dolor mauris.</p>
+          <button>See Projects</button>
+        </div>
+
+        <div className='first-image'>
+          <img src={Elipse1} alt="" />
+        </div>
+
+        <div className="frame-container first">
+          <div className='frame'>
+            <h2>10+</h2>
+            <p>Years of experience</p>
+          </div>
+        </div>
+
+        <div className="frame-container second">
+          <div className='frame'>
+            <h2>10+</h2>
+            <p>Years of experience</p>
+          </div>
+        </div>
+
 
       </div>
       <div className='header-right'>
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { links } from './Data';
+import './navbar.css'
 
 const Navbar = () => {
   return (
